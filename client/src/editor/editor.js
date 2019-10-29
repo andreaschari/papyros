6734fc76d9371f830acea7d43caa44e614f01307
Andreas Chari <2293299C@student.gla.ac.uk>
2019-10-29 21:24:51 +0000

    added new note button, started working on submit note button

--- client/src/editor/editor.js
+++ client/src/editor/editor.js
@@ -8,19 +8,36 @@ import styles from './styles';
 class EditorComponent extends React.Component{
     constructor(){
         super();
+        this.state = {
+            text: '',
+            title: '',
+            id: ''
+        };
     }
 
     render(){
 
         const { classes } = this.props;
+
         return (
         <div className={classes.editorContainer}>
-            <ReactQuill>
-                
+            <ReactQuill 
+                value = {this.state.text} 
+                onChange={this.updateBody}>
             </ReactQuill>
         </div>
         );
     }
+
+    updateBody = async(value) =>{
+        await this.setState({text: value});
+        this.update();
+    };
+
+    update = debounce(() => {
+        console.log("Updating DB")
+        // TODO
+    }, 1500);
 }
 
 export default withStyles(styles)(EditorComponent);
--- client/src/sidebar/sidebar.js
+++ client/src/sidebar/sidebar.js
@@ -8,10 +8,57 @@ import SidebarItemComponent from '../sidebarItem/sidebarItem';
 class SidebarComponent extends React.Component{
     constructor(){
         super();
+        this.state = {
+            addingNote: false,
+            title: null
+        };
+    }
+    render(){
+
+        const {notes, classes, selectedNoteIndex} = this.props;
+
+        return(
+        <div className={classes.SidebarContainer}>
+            <Button
+                onClick = {this.newNoteBtnClick}
+                className = {classes.newNoteBtn}
+            >
+                {
+                    this.state.addingNote ? 'Cancel' : 'New note'
+                }
+            </Button>
+            {
+                this.state.addingNote ? 
+                <div>
+                    <input type='text'
+                     className={classes.newNoteInput}
+                    placeholder='Enter note title.'
+                    onKeyUp = {(e) => this.updateTitle(e.target.value)}>
+                    </input>
+                    <Button 
+                    className={classes.newNoteSubmitBtn}
+                    onClick={this.newNote}>
+                        Submit Note.
+                    </Button>
+                </div> : 
+                null
+            }
+        </div>
+        );
     }
 
-    render(){
-        return(<div>Hello from Sidebar</div>);
+    newNoteBtnClick = () =>{
+        console.log("New Button Clicked!");
+        this.setState({title: null, addingNote: !this.state.addingNote});
+    }
+
+    updateTitle = (txt) =>{
+        console.log("here it is: ", txt);
+    }
+
+    newNote = () =>{
+        console.log(this.state)
+        // TODO
     }
 }
 
